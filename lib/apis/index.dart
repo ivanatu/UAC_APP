6d3abd63e29e3013f788e3ca2de5cf28bb529cec
ivanatu <ivanatu@gmail.com>
2025-03-25 11:30:24 +0300

    Update index.dart
    update ip address

--- lib/apis/index.dart
+++ lib/apis/index.dart
@@ -1,5 +1,5 @@
 class Apis {
-  static String url = "http://94.72.116.178:1337";
+  static String url = "http://207.180.224.165:1337";
   static String progress_95 = "${url}/api/progress-on-95-95-95s";
   static String messages = "${url}/api/messages?populate=*";
   static String regions = "${url}/api/regions?populate=*";
