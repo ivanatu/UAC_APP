49eaee193be8d8699e6ac0b283f9f5e8412bc52b
bruno <brunohectre@gmail.com>
2025-11-17 16:50:59 +0300

    feat: Update Accordion styling and improve myth display logic

--- lib/screens/myths_page.dart
+++ lib/screens/myths_page.dart
@@ -139,7 +139,8 @@ class _MythsScreenState extends State<MythsScreen> {
                   return noData();
                 } else {
                   return Accordion(
-                    headerBorderColor: Colors.blueGrey,
+                    headerBorderColor: widget.color,
+                    headerBackgroundColor: widget.color,
                     headerBorderColorOpened: Colors.transparent,
                     headerBackgroundColorOpened: widget.color,
                     contentBackgroundColor: Colors.white,
@@ -159,102 +160,27 @@ class _MythsScreenState extends State<MythsScreen> {
                     ) {
                       var data = mythController.mythList[index];
                       return AccordionSection(
-                        isOpen: false,
+                        isOpen: index == 0 ? true : false,
                         contentVerticalPadding: 15,
-                        header: Row(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: Image.network(
-                                Apis.url +
-                                    data.attributes.image.data.attributes.url,
-                                width: 50,
-                                height: 50,
-                                fit: BoxFit.cover,
-                                errorBuilder: (context, error, stackTrace) {
-                                  return Container(
-                                    width: 50,
-                                    height: 50,
-                                    color: Colors.grey.shade300,
-                                    child: Icon(Icons.image_not_supported),
-                                  );
-                                },
-                              ),
-                            ),
-                            SizedBox(width: 12),
-                            Expanded(
-                              child: Text(
-                                data.attributes.myth,
-                                style: Theme.of(context).textTheme.bodyMedium!
-                                    .apply(
-                                      color: Colors.white,
-                                      fontWeightDelta: 2,
-                                    ),
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ],
+                        header: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            data.attributes.myth,
+                            style: Theme.of(context).textTheme.bodyMedium!
+                                .apply(color: Colors.white, fontWeightDelta: 6),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
                         ),
                         content: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(12),
-                              child: Image.network(
-                                Apis.url +
-                                    data.attributes.image.data.attributes.url,
-                                width: double.infinity,
-                                height: 200,
-                                fit: BoxFit.cover,
-                                errorBuilder: (context, error, stackTrace) {
-                                  return Container(
-                                    width: double.infinity,
-                                    height: 200,
-                                    color: Colors.grey.shade300,
-                                    child: Center(
-                                      child: Icon(
-                                        Icons.image_not_supported,
-                                        size: 50,
-                                        color: Colors.grey.shade600,
-                                      ),
-                                    ),
-                                  );
-                                },
-                              ),
-                            ),
-                            SizedBox(height: 15),
-                            Text(
-                              "Myth",
-                              style: Theme.of(context).textTheme.titleMedium!
-                                  .apply(
-                                    fontWeightDelta: 3,
-                                    color: widget.color,
-                                  ),
-                            ),
-                            SizedBox(height: 8),
-                            Container(
-                              padding: EdgeInsets.all(12),
-                              decoration: BoxDecoration(
-                                color: Colors.grey.shade100,
-                                borderRadius: BorderRadius.circular(8),
-                                border: Border.all(color: Colors.grey.shade300),
-                              ),
-                              child: Text(
-                                data.attributes.myth,
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  height: 1.4,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ),
                             SizedBox(height: 15),
                             Text(
                               "Facts",
                               style: Theme.of(context).textTheme.titleMedium!
                                   .apply(
-                                    fontWeightDelta: 3,
+                                    fontWeightDelta: 8,
                                     color: widget.color,
                                   ),
                             ),
