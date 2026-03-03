6ed9c15ca741f845fead9960872976201fd3e9c8
bruno <brunohectre@gmail.com>
2026-03-03 23:34:36 +0300

    feat: Update API base URL and configure iOS app category type.

--- lib/apis/index.dart
+++ lib/apis/index.dart
@@ -1,6 +1,6 @@
 class Apis {
   static const String url =
-      "http://37.60.225.220:1337"; //"http://207.180.224.165:1337";
+      "http://194.163.172.6:1337"; //"http://207.180.224.165:1337";
   static String progress_95 = "${url}/api/progress-on-95-95-95s";
   static String messages = "${url}/api/messages?populate=*";
   static String regions = "${url}/api/regions?populate=*";
