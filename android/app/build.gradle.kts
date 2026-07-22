c2f6fead91f45dae2a1d29d67ff8f3680804e79c
Mugamba669 <brunohectre@gmail.com>
2026-07-22 15:30:56 +0300

    build: increment version code and configure release signing for Android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,22 +35,22 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
+        versionCode = 4
         versionName = flutter.versionName
     }
- /* signingConfigs {
+  signingConfigs {
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as String
             keyPassword = keystoreProperties["keyPassword"] as String
             storeFile = keystoreProperties["storeFile"]?.let { file(it) }
             storePassword = keystoreProperties["storePassword"] as String
         }
-    } */
+    } 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
